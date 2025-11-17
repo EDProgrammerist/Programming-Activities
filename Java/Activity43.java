d92e092a413ba727a151b75b29e4fc46ac4a1633
Ed James Ursal <paristonnn@gmail.com>
2025-11-17 11:19:36 +0800

    Update Activity43.java

--- Java/Activity43.java
+++ Java/Activity43.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class Activity43 {
-    
+
+    public static int[] getDigits(int num) {
+        int d1 = num / 1000;
+        int d2 = (num / 100) % 10;
+        int d3 = (num / 10) % 10;
+        int d4 = num % 10;
+
+        return new int[] { d1, d2, d3, d4 };
+    }
+
+    public static void printSpaced(int[] digits) {
+        System.out.println(digits[0] + " " + digits[1] + " " + digits[2] + " " + digits[3]);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("INPUT FOUR DIGIT INTEGER: ");
+
+        int num = input.nextInt();
+
+        int[] digits = getDigits(num);   
+        printSpaced(digits);             
+    }
 }
